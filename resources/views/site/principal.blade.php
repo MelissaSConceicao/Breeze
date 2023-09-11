a5f69a8b6530fb753cd01d0fc9f99a559e961233
Aluno <melissa.conceicao01@etec.sp.gov.br>
2023-09-11 08:59:49 -0300

    Rotas

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Página Principal</h1>
+    <ul>
+        <li>
+            <a href="/"> PRINCIPAL </a>
+        </li>
+        <li>
+            <a href="/sobrenos"> SOBRE NÓS </a>
+        </li>
+        <li>
+            <a href="/contato"> CONTATO </a>
+        </li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+/**
+ * Essas rotas exibem as páginas através do caminho para os 
+ * controllers e chamando as funções que vão mostrar as telas
+ */
+Route::get('/', 'App\Http\Controllers\PrincipalController@index')->name('site.principal');
+Route::get('/sobrenos', 'App\Http\Controllers\SobreNosController@sobrenos')->name('site.sobrenos');
+Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
+
+
+/**
+ * Passando parâmetros através das rotas
+ * Aqui é passado um nome e uma mensagem que pode ou não ser passada,
+ * caso não seja passada, a mensagem será 'sem texto'
+ * 
+ * Route::get('/contato/{nome}/{mensagem?}', 
+ *   function(string $nome, string $mensagem = 'sem texto')
+ *   {echo 'passagem de parâmetros via browser: ' .$nome. ' - ' .$mensagem;}
+ * );
+ */
+
+
+
+Route::prefix('/admin')-> group (function(){
+    Route::get('/clientes', function(){return 'Clientes';});
+    Route::get('/fornecedores', 'App\Http\Controllers\FornecedorController@index')->name('admin.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';});
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/admin', function(){
+    return redirect()->route('site.principal');
 });
 
-require __DIR__.'/auth.php';
+//redireciona quando não encontra parâmetros
+Route::fallback(function(){
+    echo 'a rota não exite <a href="'.route('site.principal').'"> Clique aqui </a>';
+});
+
